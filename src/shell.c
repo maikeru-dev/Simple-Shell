32261823605cd3012acc096ffb22ae31e03eff85
maikerudev <michenbox@protonmail.com>
2024-01-15 17:10:07 +0000

    Add main loop + more Gvars

--- src/shell.c
+++ src/shell.c
@@ -1,2 +1,38 @@
+#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main() { return 0; }
+int getInput(char *command) {
+  if (fgets(command, COMMAND_LENGTH, stdin) == NULL) {
+    return INPUT_TERMINATE;
+  }
+
+  if (command[0] == '\n') {
+    return INPUT_SKIP;
+  }
+
+  return INPUT_OK;
+}
+
+void quit() {
+  printf("Exiting... ");
+  exit(0);
+}
+
+int main() {
+  char command[COMMAND_LENGTH];
+  while (1) {
+    printf(PROMPT);
+
+    switch (getInput(command)) {
+    case INPUT_TERMINATE:
+      quit();
+      break;
+    case INPUT_SKIP:
+      continue;
+      break;
+    case INPUT_OK:
+      break;
+    }
+  }
+}
--- src/shell.h
+++ src/shell.h
@@ -4,3 +4,8 @@
 // Please avoid changing them.
 #define COMMAND_LENGTH 512
 #define TOKENS_LENGTH 50
+#define HISTORY_LENGTH 5
+
+#define INPUT_OK 0
+#define INPUT_TERMINATE 1
+#define INPUT_SKIP 2
